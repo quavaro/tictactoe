626443e9f937cfb9555ee00808fac33d9656bf78
Glitch (starter-react) <none>
2020-02-13 01:22:56 +0000

    👽🍲 Checkpoint ./app/components/HelloWorld.jsx:11355275/79

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -3,15 +3,16 @@ const util = require("../logic/util.js");
 const Clockface = require("./clockface.jsx");
 
 function PageRoot () {
-  const date = new Date();
+  const [date, setDate] = React.useState(0);
+  new Date();
   
   const [count, setCount] = React.useState(0);
   const doIncrement = () => {
-    setcount = count + 1;
+    setCount(count+1);
   }
   return <div><h1>This is my page now. It belongs to {util.getName("Klingon")}</h1>
     <h2>The current count is {count}</h2>
-    <button onClick=doIncrement>Increment</button>
+    <button onClick={doIncrement}>Increment</button>
   <Clockface hours={date.getHours()} minutes={date.getMinutes()} seconds={date.getSeconds()}/>
   </div>
 
