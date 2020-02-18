c43a1e1bb6076cab5db63a399a42628397b36fdc
Glitch (starter-react) <none>
2020-02-18 06:40:20 +0000

    🐷👮 Checkpoint ./app/components/Board.jsx:11355275/127 ./app/components/Tiles.jsx:11355275/75

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -1,15 +1,13 @@
 const React = require('react');
 const Tile = require('./Tiles');
-const handleClick = function handleClick(event){
-  setSymbol(symbols[turn]
-}
+
 /* takes an array prop 'tiles' and returns a <ul> element 
    with each item as <li> elements */
 const Board = function({ area }) {
   const [turn, setTurn] = React.useState(0);
   let tiles = [];
   for(let i=0; i<area; i++){
-    tiles.push(<Tile index={i} player={turn} onClick={() => setTurn((turn+1)%2)} />);
+    tiles.push(<Tile index={i} player={turn} onBoardClick={() => {setTurn((turn+1)%2); console.log('parent click');}} />);
   }
   return (
     <div className="board">{tiles}</div>
--- app/components/Tiles.jsx
+++ app/components/Tiles.jsx
@@ -1,13 +1,13 @@
 const React = require('react');
 
-const Tile = function({index, player}) {
+const Tile = function({index, player, boardClickHandler}) {
   
 
   const [symbol, setSymbol] = React.useState(' ');
   const symbols = ['X','O'];
   if(symbol == ' '){
     return (
-      <button key={index} onClick={() => {setSymbol(symbols[player])}}>{symbol}</button>
+      <button key={index} onClick={() => {setSymbol(symbols[player]); console.log('child click'); boardClickHandler();}}>{symbol}</button>
         );
   }    
   else {
