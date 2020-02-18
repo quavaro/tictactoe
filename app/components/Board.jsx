6b6f9985c99fac241cdc7b33a9ac4b3960de314f
Glitch (starter-react) <none>
2020-02-18 16:16:38 +0000

    🤑💵 Checkpoint ./app/components/Board.jsx:11355275/196

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -6,8 +6,11 @@ const Tile = require('./Tiles');
 const Board = function({ area }) {
   const [turn, setTurn] = React.useState(0);
   let tiles = [];
+  const handleBoardClick = function (event){
+    setTurn((turn+1)%2);
+  };
   for(let i=0; i<area; i++){
-    tiles.push(<Tile index={i} player={turn} onBoardClick={() => {setTurn((turn+1)%2); console.log('parent click');}} />);
+    tiles.push(<Tile index={i} player={turn} onBoardClick={handleBoardClick} />);
   }
   return (
     <div className="board">{tiles}</div>
