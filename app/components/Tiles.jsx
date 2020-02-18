e8d41c07dd4b8b9bc69e06550f764012c3018d0c
Glitch (starter-react) <none>
2020-02-18 16:28:01 +0000

    🚈🐈 Checkpoint ./app/components/Tiles.jsx:11355275/74

--- app/components/Tiles.jsx
+++ app/components/Tiles.jsx
@@ -1,13 +1,13 @@
 const React = require('react');
 
-const Tile = function({index, player, boardClickHandler}) {
+const Tile = function({index, player, onBoardClick}) {
   
 
   const [symbol, setSymbol] = React.useState(' ');
   const symbols = ['X','O'];
   if(symbol == ' '){
     return (
-      <button key={index} onClick={() => {setSymbol(symbols[player]); console.log('child click'); boardClickHandler();}}>{symbol}</button>
+      <button key={index} onClick={() => {setSymbol(symbols[player]); console.log('child click'); onBoardClick();}}>{symbol}</button>
         );
   }    
   else {
