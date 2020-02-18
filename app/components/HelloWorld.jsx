ffa241638716ea497c4e4878ba9b034847c9a93b
Glitch (languid-ant-3jvn5f9jk) <none>
2020-02-18 18:45:24 +0000

    ⛪️🔖 Checkpoint ./app/components/HelloWorld.jsx:11355275/267 ./app/components/Board.jsx:11355275/5

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -7,19 +7,20 @@ const squarea = 9;
 /* the main page for the index route of this app */
 const HelloWorld = function() {
   const [nextTurn, setNextTurn] = React.useState(0);
+  const [winner, setWinner] = React.useState("Who will win in this epic battle of wills?")
   const symbols = ['X','O'];
   const turnHandler = function() {
     setNextTurn((nextTurn+1)%2);
   }
   return (
     <div className="gameSpace">
-      <div className="info">
-        <div className="turn">It's <span className={symbols[nextTurn]}>{symbols[nextTurn]}</span>'s turn to play.</div>
-        <div className="winner"></div>
+      <div className="turn">
+        It's <span className={symbols[nextTurn]}>{symbols[nextTurn]}</span>'s turn to play.
       </div>
       <div>
         <Board area={squarea} turn={nextTurn} setTurn={turnHandler}/>
       </div>
+      <div className="winner">{winner}</div>
     </div>
   );
 }
