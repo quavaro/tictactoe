e5caacbbbacd4c817f5ed1827977a5efe5a84615
Glitch (languid-ant-3jvn5f9jk) <none>
2020-02-18 20:40:50 +0000

    🏇🐲 Checkpoint ./app/logic/utils.js:11355275/458

--- app/logic/utils.js
+++ app/logic/utils.js
@@ -1,8 +1,21 @@
+function checkWinPath(tile1, tile2, tile3) {
+  if(tile1 == 'X' && tile2 == 'X' && tile3 == 'X'){
+    return 'X';
+  }
+  else if (tile1 == 'O' && tile2 == 'O' && tile3 == 'O'){
+    return 'O';
+  }
+else return ' ';
+}
+
 function getWinner() {
   const tilesHTML = document.getElementsByTagName('button');
   let tiles = []
   for(let i=0; i<tilesHTML.length; i++){
     tiles.push(tilesHTML[i].innerHTML);
+  }
+  if(checkWinPath(tiles[0], tiles[1], tiles[2]) == ){
+    return tiles;
   }
 }
 
