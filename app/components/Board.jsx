ed3daa34f362f58ec5bbe7da54f1957fe4258547
Glitch (languid-ant-3jvn5f9jk) <none>
2020-02-19 15:57:37 +0000

    😽📚 Checkpoint ./app/components/Board.jsx:11355275/557 ./app/components/Tiles.jsx:11355275/152

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -5,11 +5,8 @@ const utils = require('../logic/utils.js');
 /* takes an array prop 'tiles' and returns a <ul> element 
    with each item as <li> elements */
 const Board = function({ area, turn, setTurn, checkWinner}) {
-    //let tiles = [];
-    const [tiles, setTiles] = React.useState([]);
-    for (let i=0; i<area; i++){
-     tiles.push(' ');
-    }
+    const [tiles, setTiles] = React.useState([' '],[' '],[' '],[' '],[' '],[' '],[' '],[' '],[' ']);
+    setTiles
     //const symbols = ['X','O'];
     //const [symbol, setSymbol] = React.useState(' ');
 
@@ -19,14 +16,12 @@ const Board = function({ area, turn, setTurn, checkWinner}) {
     };
     
     for(let i=0; i<area; i++){
-      if(turn>=0){
-        tiles.push(<Tile index={i} symbol={symbols[turn]} onBoardClick={handleBoardClick}  />);
-      }
-      //else checkWinner();
+      return (
+        <div id="board">
+          <Tile index={i} symbol={tiles[i]} onBoardClick={handleBoardClick}  />
+        </div>
+      );
     }
-    return (
-      <div id="board">{tiles}</div>
-    );
 }
 
 module.exports = Board;
--- app/components/Tiles.jsx
+++ app/components/Tiles.jsx
@@ -1,17 +1,11 @@
 const React = require('react');
 
 const Tile = function({index, symbol, onBoardClick}) {
-  if(symbol == ' ' ){
-    return (
-      <button key={index} onClick={() => { onBoardClick(event);}}>{symbol}</button>
-        );
-  }    
-  else {
+
     return (
       <button key={index} className={symbol} onClick={() => { onBoardClick(event);}}>{symbol}</button>
         );
-  }
-  
+
 }
 
 module.exports = Tile;
