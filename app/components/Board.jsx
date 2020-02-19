ea014a4c057c61f07102a396ae8a86f3ae194bbd
Glitch (languid-ant-3jvn5f9jk) <none>
2020-02-19 16:41:47 +0000

    💫🐾 Checkpoint ./app/components/Board.jsx:11355275/789

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -5,21 +5,21 @@ const utils = require('../logic/utils.js');
 /* takes an array prop 'tiles' and returns a <ul> element 
    with each item as <li> elements */
 const Board = function({ area, turn, setTurn, checkWinner}) {
-    
-    const [tiles, setTiles] = React.useState([]);
-    //const symbols = ['X','O'];
+    let tiles = [];
+    const [tilesSet, setTiles] = React.useState({0:' ',1:' ',2:' ',3:' ',4:' ',5:' ',6:' ',7:' ',8:' ',9:' '});
+
+    const symbols = ['X','O'];
     //const [symbol, setSymbol] = React.useState(' ');
 
     const handleBoardClick = function (event, index, symbol){
-      tiles = utils.getTileStates();
-      tiles[index]=symbol;
-      setTurn();
-      const currentChar = event.target.innerHTML;
-      
+      const i = index.toString();
+      setTurn();  
+      setTiles(tilesSet[index] = symbol);
+      console.log(tilesSet);
     };
     let tileMarkup= [];
     for(let i=0; i<area; i++){
-      tileMarkup.push(<Tile index={i} symbol={tiles[i] ? tiles[i] : ' ' } onBoardClick={handleBoardClick}  />);
+      tileMarkup.push(<Tile index={i} symbol={tilesSet[i]} onBoardClick={handleBoardClick}  />);
     }
       return (
         <div id="board">
